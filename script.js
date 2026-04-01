e151037aedc72e95670cd754a9a8de7dc43c8c49
henryguzman43-oss <axleguzman43@gmail.com>
2026-04-01 09:26:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let img = document.querySelector("img");
+let addButton = document.getElementById("checkBtn");
 
-fetch ("https://cataas.com/cat/says/adopt me please?json=true")
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data);
-        img.src = data.url;
-    })
- 
+addButton.addEventListener("click", function() {
+    fetch("https://cataas.com/cat/says/adopt me please?json=true")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            img.src = data.url;
+        })
+        .catch(function(error) {
+            console.log("Error:", error);
+        });
+});
